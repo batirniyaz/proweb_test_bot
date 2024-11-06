4ceea82d9c375f45980795c69500c26931a3261c
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-06 21:00:25 +0500

    configure BotUser model to admin panel

--- first_app/bot/admin.py
+++ first_app/bot/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import BotUser
+
 
 # Register your models here.
+
+
+class BotUserAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', 'username', 'role')
+    actions = ['make_admin']
+
+    def make_admin(self, request, queryset):
+        queryset.update(role='admin')
+        for user in queryset:
+            # admin_conf(user.chat_id)
+            pass
+        self.message_user(request, 'выбранные пользователи поднялись до админки')
+
