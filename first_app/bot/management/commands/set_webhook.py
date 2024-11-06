30735d77c65000b53e56b98a365e39c95c545fef
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-06 21:01:39 +0500

    create new django command: set_webhook. it sets ngrol url to webhook

---
+++ first_app/bot/management/commands/set_webhook.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+from ...bot_instance import bot
+from ...credentials import WEBHOOK_URL
+
+
+class Command(BaseCommand):
+    help = 'set webhook of telegram bot'
+
+    def handle(self, *args, **kwargs):
+        bot.set_webhook(
+            WEBHOOK_URL
+        )
