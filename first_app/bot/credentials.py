c454a8f2323a0ff523743091083ecece9a720ac6
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-06 20:59:15 +0500

    add more credentials

--- first_app/bot/credentials.py
+++ first_app/bot/credentials.py
@@ -1,3 +1,7 @@
 TOKEN = '7906298672:AAFRhFojk1xaXkwX7lyTDPk1PHXurLB97rw'
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}'
-NGROK_URL = 'https://142f-195-158-9-110.ngrok-free.app'
+NGROK_URL = 'https://5c99-192-166-230-205.ngrok-free.app'
+FEEDBACK_GROUP_ID = '-4535740331'
+
+WEBHOOK_PATH = f'bot/{TOKEN}'
+WEBHOOK_URL = f'{NGROK_URL}/{WEBHOOK_PATH}/'
