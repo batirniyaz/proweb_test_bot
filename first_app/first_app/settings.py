5a27202a512b48905a5167f30a38c45348b66d73
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-06 20:57:44 +0500

    set storage settings

--- first_app/first_app/settings.py
+++ first_app/first_app/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STORAGE_URL = '/storage/'
+
+STORAGE_ROOT = BASE_DIR / 'storage'
