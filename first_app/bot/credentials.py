9e12081a4a9de86ce5a3fb94754dbb9f85cf0990
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-07 21:05:59 +0500

    change ngrok url and add bot id

--- first_app/bot/credentials.py
+++ first_app/bot/credentials.py
@@ -1,7 +1,8 @@
 TOKEN = '7906298672:AAFRhFojk1xaXkwX7lyTDPk1PHXurLB97rw'
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}'
-NGROK_URL = 'https://ff2f-195-158-9-110.ngrok-free.app'
+NGROK_URL = 'https://9320-192-166-230-205.ngrok-free.app'
 FEEDBACK_GROUP_ID = '-4535740331'
+BOT_ID = '7906298672'
 
 WEBHOOK_PATH = f'bot/{TOKEN}'
 WEBHOOK_URL = f'{NGROK_URL}/{WEBHOOK_PATH}/'
