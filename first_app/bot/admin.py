c55dfa6839956bf2ee95c23438ba272bceda55cd
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-07 11:22:57 +0500

    configure admin panel of django

--- first_app/bot/admin.py
+++ first_app/bot/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from .handlers_admin import send_admin_conf
 from .models import BotUser
 
 
@@ -6,13 +8,18 @@ from .models import BotUser
 
 
 class BotUserAdmin(admin.ModelAdmin):
-    list_display = ('chat_id', 'username', 'role')
+    list_display = ('chat_id', 'username', 'first_name', 'role')
+    list_filter = ('language', 'role')
+    search_fields = ('chat_id', 'username', 'first_name', 'last_name')
     actions = ['make_admin']
 
     def make_admin(self, request, queryset):
         queryset.update(role='admin')
         for user in queryset:
-            # admin_conf(user.chat_id)
-            pass
+            send_admin_conf(user.chat_id)
         self.message_user(request, 'выбранные пользователи поднялись до админки')
 
+    make_admin.short_description = 'Сделать админом'
+
+
+admin.site.register(BotUser, BotUserAdmin)
