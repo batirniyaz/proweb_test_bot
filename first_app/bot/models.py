11d8c2b4779347059554ce3ea1db1efd2e933783
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-07 11:21:54 +0500

    add one more field

--- first_app/bot/models.py
+++ first_app/bot/models.py
@@ -13,16 +13,17 @@ class BotUser(models.Model):
     role = models.CharField(max_length=10, default='normal')
 
     def __str__(self):
-        return self.username or str(self.chat_id), self.first_name
+        return self.username or self.first_name
 
 
 class BotGroup(models.Model):
     chat_id = models.CharField(max_length=100, unique=True)
     chat_name = models.CharField(max_length=255, null=True, blank=True)
+    chat_link = models.CharField(max_length=255, null=True, blank=True)
     course_name = models.CharField(max_length=255)
     group_language = models.CharField(max_length=10)
     group_graphic = models.CharField(max_length=10)
     group_time = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.chat_id, self.chat_name
+        return self.chat_id, self.chat_name, self.chat_link
