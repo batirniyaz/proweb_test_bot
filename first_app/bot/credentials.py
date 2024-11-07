5b19235aca16cee84a7e41e09734a537f348ac36
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-07 11:22:40 +0500

    change ngrok url

--- first_app/bot/credentials.py
+++ first_app/bot/credentials.py
@@ -1,6 +1,6 @@
 TOKEN = '7906298672:AAFRhFojk1xaXkwX7lyTDPk1PHXurLB97rw'
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}'
-NGROK_URL = 'https://5c99-192-166-230-205.ngrok-free.app'
+NGROK_URL = 'https://ff2f-195-158-9-110.ngrok-free.app'
 FEEDBACK_GROUP_ID = '-4535740331'
 
 WEBHOOK_PATH = f'bot/{TOKEN}'
