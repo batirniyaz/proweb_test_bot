a5a371d081fa03238d89ac956cdaa0b4a30a792a
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-07 21:05:30 +0500

    create messages for uzbek version in messages dir

---
+++ first_app/bot/messages/messages_uz.py
@@ -0,0 +1,123 @@
+from ..credentials import NGROK_URL
+
+welcome_message = """
+
+<b>PROWEB</b> zamonaviy kasblar markaziga xush kelibsiz! 🤗
+    """
+
+help_message = """
+
+Bu bot maxsus <b>PROWEB</b> markazi talabalari uchun yaratilgan.
+
+Bot imkoniyatlari:
+
+■ <b>Matnli xabar yuboring</b> - mashg'ulotingiz bilan bog'liq har qanday savolni bering va iloji boricha tezroq qayta aloqa oling.
+
+■ <b>Texnik yordam</b> - texnik yordam bilan bog'laning va kompyuter texnologiyalari bilan bog'liq muammolarni hal qiling. Shuningdek, siz shaxsiy kompyuterni yig'ish bo'yicha maslahat va yordam oling.
+
+■ <b>Kovorking</b> - kovorking administratoriga murojaat qiling va tashrif buyurishga joyni bron qiling.
+
+■ <b>Tanlovlar</b> - har oylik tanlovlarda qatnashib, qimmatbaho sovrinlarni yutib oling.
+
+■ <b>Saytga tashrif buyuring</b> - PROWEB markazining sahifasiga o'ting, u erda siz markaz va mavjud kurslar haqidagi barcha batafsil ma'lumotlarni topasiz.
+
+■ <b>Kompyuter asoslari</b> - kompyuter asoslari kursiga yozilish.
+
+■ <b>Sharh qoldiring</b> - PROWEB markazida mashg'ulotlardan yoqimli taassurotlar bilan bo'lishish imkoniyati. Agar qiyinchiliklarga duch kelsangiz, shikoyat yoki so'rov qoldirishingiz mumkin. Biz har bir fikr uchun minnatdormiz.
+
+■ <b>O'qish qoidalari</b> - PROWEB markazida o'qish qoidalari haqida batafsil ma'lumot.
+
+■ <b>Bosh sahifaga</b> - botning asosiy sahifasiga qaytish va imkoniyatlari haqida batafsil ma'lumot olish.
+
+■ <b>O'zbek tili</b> - tilni o'zbek tilidan rus tiliga o'tkazish.
+
+■ <b>Kontakt bilan ulashing</b> - agar sizning savolingiz shaxsiy xarakterga ega bo'lsa, o'z ma'lumotlaringizni qoldiring va markaz ma'muriyati imkon qadar tezroq siz bilan shaxsan bog'lanadi.
+"""
+
+conkurs_message = f"""
+
+Qimmatbaho sovrinli tanlovlar har oy <b>PROWEB</b> markazi o'quvchilari uchun o'tkaziladi! 🎉
+
+Tanlovlarda qatnashish uchun botimizga obuna bo'lishingiz kerak.
+
+Tanlov shartlari ko'rsatilgan batafsil xabarlar barcha obunachilarga shaxsiy xabar orqali yuboriladi. Obunani saqlab qolish uchun botni to'xtatmaslik yoki bloklamasligingiz kifoya.
+
+Shuningdek, biz Instagram, Youtube, Telegram, Facebookdagi obunachilarimiz uchun qo'shimcha tanlovlar o'tkazamiz - obuna bo'ling va bir vaqtning o'zida bir nechta tanlovlarda ishtirok eting.
+
+Hammaga omad, har bir talabada g'alaba qozonish imkoniyati bor! 🍀
+
+<a href="{NGROK_URL}/storage/conkurs.jpg">\u00A0</a>
+    """
+
+basic_course_message = f"""
+
+Sizda umuman kompyuter bilimlari yo'q, lekin <b>PROWEB</b>da o'qishni xohlaysizmi? Bu bizning kompyuter asoslari kursimiz bilan mumkin. Buning uchun "<b>Kompyuter asoslari kursiga yozilish</b>" tugmasini bosish kifoya va o'qituvchimiz sizga hamma narsa haqida batafsil maslahat beradi.
+
+<a href="{NGROK_URL}/storage/basic_course.jpg">\u00A0</a>
+    """
+
+review_message = """
+
+Sizga <b>PROWEB</b> markazida o'qish yoqadimi va bu haqda ijtimoiy tarmoqlarimizda bo'lishish siz uchun qiyin emasmi? Bo'lmasam "<b>Sharhlar</b>" havolasiga o'ting va taassurotlaringizni ta'riflang, biz sizga juda minnatdor bo'lamiz.
+
+Agar siz mashg'ulot paytida qiyinchiliklarga duch kelsangiz yoki sizga biror narsa yoqmagan bo'lsa, ehtimol sizda taklif va istaklar bor bo'lsa? Bo'lmasam "<b>Shikoyat va istaklar</b>" havolasiga o'ting, hamma narsani batafsil tavsiflab bering va biz o'quv markazimizni yaxshilash uchun qo'limizdan kelganini qilamiz.
+    """
+
+licence_message = """
+
+<b>PROWEB o'quv markazida o'qish qoidalari</b>
+
+1. Bir o'quv oyida - 8 ta dars. Bir oy o'qish uchun pul to'lagan o'quvchilarga tasdiqlangan jadval bo'yicha 8 ta dars o'tkazilishi kafolatlanadi.
+
+2. Agar dars bayram yoki o'qituvchi tomonidan uzrli sabablarga ko'ra bekor qilinsa, dars hech qanday holatda kuymaydi, balki keyinga qoldiriladi. Bir akademik oyda 8ta dars bor. Barcha 8ta rasmiy darslar bajarilmaguncha, o'quv oyi tugamaydi.
+
+3. Agar o'quvchi rejalashtirilgan darsni qoldirsa, bu dars hisobga olinadi, chunki har bir dars video formatida Telegramdagi guruhga jo'natiladi.
+
+4. Agar o'quvchi darsga qatnasha olmasa, u bu haqida o'qituvchi va ma'muriyatga xabar berishi shart.
+
+5. Agar o'quvchi u holda har tomonlama o'qituvchilar joylashgan kovorking xonaga tashrif buyurish tavsiya etiladi, u yerda har bir yo'nalishdan o'qituvchilar bo'ladi. Kovorking o'qituvchilari o'tkazib yuborilgan mavzuni va o'qishdagi qiyinchiliklari bilan yordam berishlari shart.
+6. O'qish mobaynida o'quvchi har kuni kovorking xonasiga mavjud vaqtga oldindan ro'yxatdan o'tib, tashrif buyurishi mumkin. Ro'yxatdan o'tish uchun kovorking o'qituvchisiga murojaat qilishi va joy bron qilishi kerak.
+
+7. Kursni boshlashdan oldin yoki mashg'ulot paytida kompyuter bilimi kam bo'lgan o'quvchilarga bepul kompyuter asoslari kursiga yozilish tavsiya etiladi. Bir oy ichida o'quvchilar kompyuter savodxonligi asoslarini o'rganadilar. O'qish asosiy kursga parallel ravishda o'tkaziladi. Ro'yxatdan o'tish uchun siz Telegramda kompyuter asoslari kursi o'qituvchisiga murojaat qilishingiz kerak.
+
+8. PROWEB o'quv markazining har bir o'quvchisi, guruhdan chetlatilishining oldini olish uchun, o'quv oyining boshida o'qish oyi uchun to'lovni o'z vaqtida to'lashi kerak. Agar o'quvchi moliyaviy qiyinchiliklarga duch kelsa, ma'muriyat har haftalik to'lashga imtiyozli davrni taqdim etishi mumkin yoki o'quvchi joriy o'qish davriga qisman to'lashi mumkin.
+
+9. Agar o'quvchi o'z vaqtida to'lamasa va uzrli sababsiz uchdan ortiq darsda qatnashmasa, bu o'quvchi avtomatik ravishda guruhdan chiqariladi. Guruhga qayta tiklanish uchun o'quvchi qarzini to'lashi kerak, shundan keyingina u o'z guruhiga qaytariladi, agar u guruh oldinga o'tib ketgan bo'lmasa. Yoki ma'muriyat ushbu o'quvchiga eng mos keladigan guruhni tanlab beradi.
+
+10. Agar o'quvchi o'qishni to'xtatishni xohlasa, u ma'muriyatni xabardor qilishi va sababini tushuntirishi kerak, shunda o'quvchi kelmaydigan darslar hisobga olinmaydi. Ma'muriyat mashg'ulotlarni o'z vaqtida to'xtatib qo'yadi, o'quvchini Telegram guruhidan chiqaradi va mashg'ulotlarni to'xtatadi.
+
+11. O'qishni to'xtatgan o'quvchi, o'qishni to'xtatgan oyi va darsidan boshlab davom ettirishi mumkin. Ma'muriyat uni o'z o'qituvchisiga qaytarilishini kafolatlamaydi.
+
+12. Agar o'quvchi o'qish formatini, o'qituvchini yoki mashg'ulot vaqti va kunlarini o'zgartirmoqchi bo'lsa, u markaz ma'muriyati bilan bog'lanishi mumkin, ular albatta bu istaklar bilan yordam berishadi.
+
+13. Shikoyat yoki taklifga ega bo'lgan o'quvchi qayta aloqa menejeri yoki o'quv markazi ma'muriyatiga murojaat qilishi mumkin. Barcha shikoyat va takliflar albatta ko'rib chiqiladi va tegishli choralar ko'riladi.
+
+<b>Kontaktlar:</b>
+
+<b>Markaz ma'muriyati</b>
+Telegram: t.me/proweb_admin 
+Tel: +97 744 66 32
+
+<b>Qayta aloqa menejeri</b>
+Telegram: t.me/proweb_lost_back
+Tel: +88 198 66 32
+
+<b>Kovorking</b>
+Telegram: t.me/proweb_coworking 
+Tel: +97155 66 32
+
+<b>Kompyuter asoslari kursi</b>
+Telegram: t.me/proweb_basics
+
+    """
+
+complain_suggestion_message = """
+
+Qanday qiyinchiliklarga duch keldingiz yoki <b>PROWEB</b> markazini takomillashtirish bo'yicha qandaytakliflaringiz bor?
+
+Iltimos, “<b>Kontaktn bilan ulashing</b>” tugmasini bosish orqali kontaktingizni baham koʻring, bizning maʼmuriyatimiz siz bilan shaxsan bogʻlanadi va barcha fikr-mulohazalaringizni tinglaydi yoki batafsilroq tavsiflab bering va xabar qilib yuboring.
+    """
+
+text = 'Biz sizning matningizni oldik va uni imkon qadar tezroq qayta ishlashga harakat qilamiz'
+
+phone = 'Biz sizning ma\'lumotlaringizni oldik va tez orada siz bilan bog\'lanamiz'
