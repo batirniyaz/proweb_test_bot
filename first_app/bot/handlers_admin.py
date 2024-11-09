1513cfa4f6039693daab813d27f83189d3c9d25b
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-09 21:28:38 +0500

    a lot of changes

--- first_app/bot/handlers_admin.py
+++ first_app/bot/handlers_admin.py
@@ -1,24 +1,9 @@
 from telebot import types
-import telebot
-
-from . import messages
 
 from .bot_instance import bot
 from .models import BotUser, BotGroup
 
-selected_courses = {}
-
-
-class IsAdmin(telebot.custom_filters.SimpleCustomFilter):
-    key = 'is_admin'
-
-    @staticmethod
-    def check(message: telebot.types.Message):
-        try:
-            user = BotUser.objects.get(chat_id=message.chat.id)
-        except BotUser.DoesNotExist:
-            return False
-        return True if user.role == 'admin' else False
+admin_selection = {}
 
 
 def send_admin_conf(chat_id):
@@ -66,6 +51,10 @@ def admin_panel(message: types.Message):
 
 @bot.message_handler(is_admin=True, func=lambda message: message.text == 'Отправить' or message.text == 'Переслать')
 def on_click_admin(message: types.Message):
+    cid = message.chat.id
+    admin_selection.setdefault(cid, [{'command': ''}, {'language': ''}, {'courses': []}, {'messages': []}])
+    admin_selection[cid][0]['command'] = message.text
+    print(admin_selection)
     if message.text == 'Отправить':
         lang_select(message)
     elif message.text == 'Переслать':
@@ -79,26 +68,37 @@ def lang_select(message: types.Message):
     ru_btn = types.KeyboardButton('РУС')
     uz_btn = types.KeyboardButton('УЗБ')
     all_btn = types.KeyboardButton('Все')
-    keyboard.row(ru_btn, uz_btn)
-    keyboard.add(all_btn)
+    keyboard.row(ru_btn, uz_btn, all_btn)
+    keyboard.add(types.KeyboardButton('Отменить'))
 
     bot.send_message(message.chat.id, ' Язык?', reply_markup=keyboard)
     bot.register_next_step_handler(message, lang_handle)
 
 
-@bot.message_handler(is_admin=True, func=lambda message: message.text in ['РУС', 'УЗБ', 'Все'])
-def lang_handle(message: types.Message):
-    if message.text == 'Все':
+def groups_by_lang(m: types.Message):
+    if m.text == 'Все':
         groups = BotGroup.objects.all()
     else:
-        groups = BotGroup.objects.filter(group_language=message.text)
-    course_select(message, groups)
+        groups = BotGroup.objects.filter(group_language=m.text)
+    return groups
+
+
+@bot.message_handler(is_admin=True, func=lambda message: message.text in ['РУС', 'УЗБ', 'Все', 'Отменить'])
+def lang_handle(message: types.Message):
+    cid = message.chat.id
+    if message.text != 'Отменить':
+        admin_selection[cid][1]['language'] = message.text
+        print(admin_selection)
+        groups = groups_by_lang(message)
+        course_select(message, groups)
+    else:
+        del admin_selection[cid]
+        admin_panel(message)
 
 
 def create_inline_kb(kbs, chat_id, lang):
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-
-    selected = selected_courses.get(chat_id, [])
+    keyboard = types.InlineKeyboardMarkup()
+    selected = admin_selection[chat_id][2]['courses']
 
     for inline in kbs:
         text = f'✔️ {inline}' if inline in selected else inline
@@ -114,7 +114,7 @@ def create_inline_kb(kbs, chat_id, lang):
 @bot.callback_query_handler(func=lambda call: call.data.startswith('select_') or call.data == 'confirm')
 def multiple_selection(call: types.CallbackQuery):
     chat_id = call.message.chat.id
-    selected = selected_courses.setdefault(chat_id, [])
+    selected = admin_selection[chat_id][2]['courses']
 
     if call.data != 'confirm':
         msg = call.data[7:]
@@ -135,18 +135,20 @@ def multiple_selection(call: types.CallbackQuery):
             else:
                 selected.append(course)
 
-        all_groups = set(group.course_name for group in BotGroup.objects.filter(group_language=lang))
-        keyboard = create_inline_kb(all_groups, chat_id, lang)
+        call.message.text = lang
+        groups = groups_by_lang(call.message)
+        keyboard = create_inline_kb(set(group.course_name for group in groups), chat_id, lang)
         bot.edit_message_reply_markup(chat_id=chat_id, message_id=call.message.message_id, reply_markup=keyboard)
 
     else:
+        bot.delete_message(chat_id, message_id=call.message.message_id)
+        bot.delete_message(chat_id, message_id=call.message.message_id+1)
         if 'All' in selected:
-            # selected_courses[chat_id] = ['All']
             bot.send_message(chat_id, f'Принял ваш запрос. Что вы хотите отправить во все группы')
         else:
-            bot.send_message(chat_id, f'Принял ваш запрос. Что вы хотите отправить в группу(ы) {", ".join(selected_courses[chat_id])}')
+            bot.send_message(chat_id,
+                             f'Принял ваш запрос. Что вы хотите отправить в группу(ы) {", ".join(admin_selection[chat_id][2]["courses"])}')
 
-        del selected_courses[chat_id]
         bot.register_next_step_handler(call.message, get_messages)
 
     bot.answer_callback_query(callback_query_id=call.id)
@@ -155,16 +157,67 @@ def multiple_selection(call: types.CallbackQuery):
 def course_select(message: types.Message, groups):
     group_filter_by_course = set(group.course_name for group in groups)
     keyboard = create_inline_kb(group_filter_by_course, message.chat.id, message.text)
-
+    box_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    box_keyboard.add(types.KeyboardButton('Отменить'))
     bot.send_message(message.chat.id, 'Выберите курс', reply_markup=keyboard)
+    bot.send_message(message.chat.id, 'Или отмените и начните заново', reply_markup=box_keyboard)
 
 
-@bot.message_handler(is_admin=True, content_types=['text', 'audio', 'video', 'image', 'photo', 'voice', 'document'], func=lambda message: True)
+@bot.message_handler(is_admin=True, content_types=['text', 'audio', 'video', 'image', 'photo', 'voice', 'document'],
+                     func=lambda message: True)
 def get_messages(message: types.Message):
-    bot.send_message(message.chat.id, message.text)
+    cid = message.chat.id
+    if message.text == 'Подтвердить':
+        filter_groups(message)
+    elif message.text == 'Отменить':
+        del admin_selection[cid]
+        admin_panel(message)
+    else:
+        msgid = message.id
+        msg = message.html_text
+        admin_selection[cid][3]['messages'].append({msgid: msg})
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(types.KeyboardButton('Подтвердить'), types.KeyboardButton('Отменить'))
+        bot.send_message(message.chat.id, 'Принял. Еще?', reply_markup=keyboard)
+        print(admin_selection)
+        bot.register_next_step_handler(message, get_messages)
 
 
+def filter_groups(message: types.Message):
+    cid = message.chat.id
 
+    com = admin_selection[cid][0]['command']
+    lang = admin_selection[cid][1]['language']
+    crs = admin_selection[cid][2]['courses']
+    msgs = admin_selection[cid][3]['messages']
 
+    print(f'{com=}, {lang=}, {crs=}, {msgs=}')
 
-bot.add_custom_filter(IsAdmin())
+    if lang != 'Все':
+        print('here')
+        groups = BotGroup.objects.filter(group_language=lang)
+    else:
+        groups = BotGroup.objects.all()
+
+    filtered_groups = []
+    if 'All' not in crs:
+        for course in crs:
+            temp_groups = groups.filter(course_name=course)
+            for temp_group in temp_groups:
+                filtered_groups.append(temp_group)
+    else:
+        temp_groups = groups.all()
+        for temp_group in temp_groups:
+            filtered_groups.append(temp_group)
+    if com == 'Отправить':
+        for group in filtered_groups:
+            for msg in msgs:
+                bot.send_message(group.chat_id, msg.values(), parse_mode='html')
+    else:
+        for group in filtered_groups:
+            for msg in msgs:
+                bot.forward_message(group.chat_id, message.from_user.id, msg.keys())
+
+    bot.send_message(cid, 'Готово')
+    del admin_selection[cid]
+    admin_panel(message)
