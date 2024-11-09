c23ffa4a5a666343472d039e5fa295cd1be73637
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-09 19:58:23 +0500

    set is admin filter to instance of bot

--- first_app/bot/bot_instance.py
+++ first_app/bot/bot_instance.py
@@ -1,6 +1,8 @@
 import telebot
 from .credentials import TOKEN
-
+from .filter import IsAdmin
 
 API_TOKEN = TOKEN
 bot = telebot.TeleBot(API_TOKEN, threaded=False)
+
+bot.add_custom_filter(IsAdmin())
